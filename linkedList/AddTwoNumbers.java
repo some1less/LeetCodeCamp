06f8a37397de6b06ed09bd8aba698e10df502923
some1less <muracks.max@gmail.com>
2025-08-25 21:57:10 +0200

    25.08

--- linkedList/AddTwoNumbers.java
+++ linkedList/AddTwoNumbers.java
@@ -1,4 +1,67 @@
 package linkedList;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class AddTwoNumbers {
+
+    /*
+    You are given two non-empty linked lists representing two non-negative integers.
+    The digits are stored in reverse order, and each of their nodes contains a single digit.
+    Add the two numbers and return the sum as a linked list.
+
+    You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+    */
+
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+
+    public static void main(String[] args) {
+
+        ListNode a = new ListNode(2, new ListNode(4, new ListNode(3)));
+        ListNode b = new ListNode(5, new ListNode(6, new ListNode(4)));
+        ListNode res = addTwoNumbers(a, b);
+
+        while (res != null) {
+            System.out.print(res.val +" ");
+            res = res.next;
+        }
+
+
+    }
+
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        ListNode head = new ListNode(0);
+        ListNode cur = head;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+            int x = (l1 != null) ? l1.val : 0;
+            int y = (l2 != null) ? l2.val : 0;
+
+            int sum = x + y + carry;
+            carry = sum / 10;
+
+            cur.next = new ListNode(sum % 10);
+            cur = cur.next;
+
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+        return head.next;
+    }
+
+    // SC: O(1)
+    // TC: O(max(n,m))
+
+
+
 }
